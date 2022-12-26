ddffe49cc7a4f1403835852398d186b036f96fc6
PrometheusAnith <120585581+PrometheusAnith@users.noreply.github.com>
2022-12-26 16:59:42 +0530

    Add files via upload

---
+++ ASSIGNMENT 2 QUESTION 2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program to print a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
+
+# In[10]:
+
+
+OUTPUT={}
+OUTPUT['a']=(ord('a'))
+OUTPUT['b']=(ord('b'))
+OUTPUT['c']=(ord('c'))
+OUTPUT['d']=(ord('d'))
+OUTPUT['e']=(ord('e'))
+OUTPUT['f']=(ord('f'))
+OUTPUT['g']=(ord('g'))
+OUTPUT['h']=(ord('h'))
+OUTPUT['i']=(ord('i'))
+OUTPUT['j']=(ord('j'))
+OUTPUT['k']=(ord('k'))
+OUTPUT['l']=(ord('l'))
+OUTPUT['m']=(ord('m'))
+OUTPUT['n']=(ord('n'))
+OUTPUT['o']=(ord('o'))
+OUTPUT['p']=(ord('p'))
+OUTPUT['q']=(ord('q'))
+OUTPUT['r']=(ord('r'))
+OUTPUT['s']=(ord('s'))
+OUTPUT['t']=(ord('t'))
+OUTPUT['u']=(ord('u'))
+OUTPUT['v']=(ord('v'))
+OUTPUT['w']=(ord('w'))
+OUTPUT['x']=(ord('x'))
+OUTPUT['y']=(ord('y'))
+OUTPUT['z']=(ord('z'))
+print(OUTPUT)
+
+
+# In[ ]:
+
+
+
+
